7d065843cb186ae8c735bdd8725c7a146822213c
Herman Mwashighadi <hmwashigadi@gmail.com>
2021-04-21 09:58:53 +0300

    Navigation links and pages apps creation

--- classyshoes/settings.py
+++ classyshoes/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'pages.apps.PagesConfig', # pages application
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'classyshoes/static'),
+]
