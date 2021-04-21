cf02ba0f855de464021899731a7c8a3c219f1b12
Herman Mwashighadi <hmwashigadi@gmail.com>
2021-04-21 15:21:03 +0300

    Teams Model complete

--- classyshoes/settings.py
+++ classyshoes/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'classyshoes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'classyshoes_db',
+        'USER': 'postgres',
+        'PASSWORD': 'W#E$w3e4',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -123,3 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'classyshoes/static'),
 ]
+
+
+# media settings 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
